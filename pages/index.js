ccdd9b6d16d61ad9d7bd17e739bcdc0f535d46b8
Rohit Singh <rohit99.ind@gmail.com>
2024-04-29 18:47:34 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,74 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+function mdxTableToHtml(mdxTable) {
+  const rows = mdxTable.split("\n");
+  let html = "<table>\n";
+  let headerRow = true;
+  rows.splice(1, 1);
+
+  for (const row of rows) {
+    const cells = row.split("|").slice(1, -1);
+
+    html += "  <tr>\n";
+
+    for (const cell of cells) {
+      if (headerRow) {
+        html += "    <th>" + cell.trim() + "</th>\n";
+      } else {
+        html += "    <td>" + cell.trim() + "</td>\n";
+      }
+    }
+
+    headerRow = false;
+    html += "  </tr>\n";
+  }
+
+  html += "</table>";
+
+  return html;
+}
+// function mdxTableToHtml(mdxTable) {
+//   const rows = mdxTable.split("\n");
+//   let html = "<table>\n";
+//   let headerRow = true;
+
+//   rows.splice(1, 1);
+//   for (const row of rows) {
+//     if (row.startsWith("|") && row.endsWith("|")) {
+//       const cells = row.split("|").slice(1, -1);
+
+//       html += "  <tr>\n";
+
+//       for (const cell of cells) {
+//         if (headerRow) {
+//           html += "    <th>" + cell.trim() + "</th>\n";
+//         } else {
+//           html += "    <td>" + cell.trim() + "</td>\n";
+//         }
+//       }
+
+//       headerRow = false;
+//       html += "  </tr>\n";
+//     } else if (row.startsWith("|")) {
+//       headerRow = true;
+//     }
+//   }
+
+//   html += "</table>";
+
+//   return html;
+// }
 
 export default function Home() {
+  const [mdxTable, setMdxTable] = useState(`| 1 | 2 | 3 |`);
+  const [htmlTable, setHtmlTable] = useState(mdxTableToHtml(mdxTable));
+  useEffect(() => {
+    setHtmlTable(mdxTableToHtml(mdxTable));
+  }, [mdxTable]);
   return (
     <>
       <Head>
@@ -15,109 +78,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            height: "100vh",
+          }}
+        >
+          <textarea
+            style={{ width: "100%", height: "100%" }}
+            onChange={(e) => setMdxTable(e.target.value)}
+            value={mdxTable}
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          <textarea
+            style={{ width: "100%", height: "100%" }}
+            value={htmlTable}
+          />
         </div>
       </main>
     </>
-  )
+  );
 }
